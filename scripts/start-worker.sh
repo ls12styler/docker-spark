6ef4a6e4da670b332a69d23329fd9830a648aff5
Ashley Broadley <ashley@ls12style.co.uk>
2018-12-11 08:59:42 +0000

    Using the right ENV var in start-worker for the Spark Master

--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -8,4 +8,4 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
-spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_WORKER >> $SPARK_WORKER_LOG/spark-worker.out
+spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
